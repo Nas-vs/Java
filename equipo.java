86ad430c216cc78c4a62509cdb2e2d1a67366b83
Nas-vs <125238212+Nas-vs@users.noreply.github.com>
2023-10-31 15:01:10 -0500

    Add files via upload

---
+++ equipo.java
@@ -0,0 +1,19 @@
+
+public class equipo {
+
+	private int idequipo;
+	private String nombre;
+	public int getIdequipo() {
+		return idequipo;
+	}
+	public void setIdequipo(int idequipo) {
+		this.idequipo = idequipo;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+}
